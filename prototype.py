c4d94805ab1125554170ad99ae453980aa803428
DeveloperSteve <developersteve@DeveloperSteves-MacBook-Pro-9.local>
2024-05-04 13:49:38 +1000

    fixed the json output and made the 3x3 grid more dynamic

--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
-import numpy as np
 import os
+import numpy as np
+import json
 
 # Get grid size from environment variable or default to 5
 grid_size = int(os.getenv('GRID_SIZE', '5'))
@@ -16,8 +17,9 @@ class GridWorld:
         self.size = size
         self.reward = np.zeros((size, size))
 
-        # Define rewards for each cell
-        self.goal = (3, 3)        
+        # Set goal to bottom-right corner of the grid
+        self.goal = (size - 1, size - 1)        
+        
         self.state = None
         self.reset()
 
@@ -33,32 +35,25 @@ class GridWorld:
     def step(self, action):
         row, col = self.state
 
-        if action == Action.RIGHT.value:  # Move right
+        if action == Action.RIGHT.value:
             col = min(col + 1, self.size - 1)
-        elif action == Action.LEFT.value:  # Move left
+        elif action == Action.LEFT.value:
             col = max(col - 1, 0)
-        elif action == Action.DOWN.value:  # Move down
+        elif action == Action.DOWN.value:
             row = min(row + 1, self.size - 1)
-        elif action == Action.UP.value:  # Move up
+        elif action == Action.UP.value:
             row = max(row - 1, 0)
 
         self.state = (row, col)
         reward_value = self.get_reward(self.state)
-        done = self.state == self.goal  # Terminate if reached the goal
+        done = self.state == self.goal
         return self.state, reward_value, done, {}
 
     def render(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if (i, j) == self.state:
-                    print("x", end=" ")
-                elif (i, j) == self.goal:
-                    print("G", end=" ")
-                else:
-                    print("-", end=" ")
-            print()
-        print()
-        print()
+        grid = [['-' for _ in range(self.size)] for _ in range(self.size)]
+        grid[self.state[0]][self.state[1]] = 'x'
+        grid[self.goal[0]][self.goal[1]] = 'G'
+        return json.dumps(grid)
 
 class QTable:
     def __init__(self, row, col, num_actions):
@@ -73,17 +68,12 @@ class QTable:
 class SarsaAgent:
     def __init__(self, num_actions, alpha=0.1, gamma=0.99, epsilon=0.1):
         self.num_actions = num_actions
-        
-        # NFT hyperperameters
-        self.alpha = alpha  # Learning rate
-        self.gamma = gamma  # Discount factor
-        self.epsilon = epsilon  # Exploration rate
-        
-        # Learning
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
         self.last_state = None
         self.last_action = None
 
-    # Engine
     def set_q_table(self, q_table):
         self.q_table = q_table
 
@@ -97,11 +87,10 @@ class SarsaAgent:
     def learn(self, state, action, reward, next_state, next_action):
         next_row, next_col = next_state        
         if self.last_state is not None:
-            # Update Q-value using SARSA
             current_q = self.q_table.get_value(next_row, next_col, next_action)
-            last_q = self.q_table.values[self.last_state, self.last_action]
+            last_q = self.q_table.values[self.last_state][self.last_action]
             update = self.alpha * (reward + self.gamma * current_q - last_q)
-            self.q_table.values[self.last_state, self.last_action] += update
+            self.q_table.values[self.last_state][self.last_action] += update
         self.last_state = next_state
         self.last_action = next_action
 
@@ -118,29 +107,24 @@ def train_agent(env, agent, num_episodes=100, render=False):
 
         while not done:
             if render:
-                env.render()
+                print(env.render())  # Print JSON output for debugging or interfacing
             next_state, reward, done, _ = env.step(action)
             next_action = agent.choose_action(next_state)
             agent.learn(state, action, reward, next_state, next_action)
             state = next_state
             action = next_action
     if render:
-        env.render()
+        print(env.render())  # Final render in JSON
 
 # Define environment and agent
-# print("Starting Gridworld")
 env = GridWorld(grid_size)
-env.render()
+print(env.render())  # Print initial state as JSON for checking
 
 ns = grid_size * grid_size
 na = len(list(Action))
 print(ns, na)
 
 q = QTable(grid_size, grid_size, na)
-
 agent = SarsaAgent(na)
 agent.set_q_table(q)
-
-# Train the agent
 train_agent(env, agent, 1)
-# print("Finished training")
