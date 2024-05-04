c88b9b39dbd62b0b077018db0e4924b66e52ab81
Dougy Lee <hello@androiddougy.com>
2024-05-04 22:38:35 +1000

    Add mining page scaffold

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Scaffold from "./Scaffold";
+import Mine from "./Mine";
+import Home from "./Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Scaffold />,
+        element: <Home />,
+    },
+    {
+        path: "mine",
+        element: (
+            <Scaffold>
+                <Mine row={2} col={3} />
+            </Scaffold>
+        ),
     },
 ]);
 
