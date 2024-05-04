e1eee68d89f9ba671bb0dfdd92b749aab2480242
Dougy Lee <hello@androiddougy.com>
2024-05-04 13:54:26 +1000

    Add foundry

--- nft/src/Sarsa.sol
+++ nft/src/Sarsa.sol
@@ -2,45 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-// Interface for OpenSea
-interface IERC1155MetadataURI {
-    function uri(uint256 _id) external view returns (string memory);
-}
-
-contract NFT is ERC1155 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
+contract Sarsa is ERC1155 {
     // IPFS content for each NFT
+    string public symbol;
     mapping(uint256 => string) public images;
     mapping(uint256 => string) public brains;
+    uint256 public tokenCount;
 
-    constructor() ERC1155("https://api.opensea.io/api/asset/{id}") {}
+    constructor() ERC1155("https://api.opensea.io/api/asset/{id}") {
+        tokenCount = 0;
+        symbol = "SARSA";
+    }
 
     function mint(
         address account,
         string memory _imageURI,
         string memory _brainURI
     ) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-
+        uint256 newItemId = tokenCount++;
         images[newItemId] = _imageURI;
         brains[newItemId] = _brainURI;
 
         _mint(account, newItemId, 1, "");
-        _setTokenURI(newItemId, _imageURI); // Set the URI for OpenSea
-        return newItemId;
+        // _setTokenURI(newItemId, _imageURI); // Set the URI for OpenSea
+        return 1;
     }
 
-    // Override function to include URI for OpenSea
-    function uri(uint256 _id) public view override returns (string memory) {
-        require(
-            _exists(_id),
-            "ERC1155Metadata: URI query for nonexistent token"
-        );
-        return super.uri(_id);
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        return images[tokenId];
     }
 }
